d4bfc24cfd60dd9891be8eae5a944466448e359c
GeriejeydEspina <146457284+GeriejeydEspina@users.noreply.github.com>
2023-10-27 15:36:06 +0800

    Update courses.js

--- courses.js
+++ courses.js
@@ -12,31 +12,15 @@ const text = " Code your dreams into reality with Programming World!";
                 
                     typingInterval = setInterval(type, 100);
 
-const slider = document.querySelector('.slider');
-                const slides = document.querySelectorAll('.slide');
-                const prevBtn = document.querySelector('.prev-btn');
-                const nextBtn = document.querySelector('.next-btn');
-                let slideIndex = 0;
-                    nextBtn.addEventListener('click', () => {
-                        slideIndex++;
-                    if (slideIndex >= slides.length) {
-                        slideIndex = 0;
-                    }
-                    updateSlider();
-                    });
-                    prevBtn.addEventListener('click', () => {
-                        slideIndex--;
-                    if (slideIndex < 0) {
-                        slideIndex = slides.length - 1;
-                    }
-                    updateSlider();
-                    });
-                function updateSlider() {
-                    const translateValue = -slideIndex * 100 + '%';
-                        slider.style.transform = `translateX(${translateValue})`;
-                }
 
-window.onmousemove = function(e){
-                    let x = e.clientX/3;
-                    box.style.transform = "perspective(1000px) rotateY(" + x + "deg)";
-                }
+                    
+                    function updateSlider() {
+                        const translateValue = -slideIndex * 100 + '%';
+                            slider.style.transform = `translateX(${translateValue})`;
+                    }
+                    
+                    let box = document.querySelector('.box');
+                    window.onmousemove = function(e){
+                        let x = e.clientX/3;
+                        box.style.transform = "perspective(1000px) rotateY(" + x + "deg)";
+                    }
